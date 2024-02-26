60ad27b9c8bbf5a5c95183b4bd01781ad17d475c
petersonguo <petersonguo@gmail.com>
2024-02-25 23:34:26 -0500

    Fix build

--- app/about/page.js
+++ app/about/page.js
@@ -11,7 +11,7 @@ export default function Page() {
                     About Me
                 </h1>
                 <div>
-                    I'm an Electrical Engineering graduate from the University of Waterloo, specializing in Artificial Intelligence. My passion for technology drives my continuous exploration of programming languages and frameworks. With experience in security, full-stack development, and robotics, I have contributed to diverse projects, showcasing my ability to innovate and solve complex problems. My academic and practical experiences have equipped me with a strong foundation to contribute effectively to technology-driven environments. My goal is to leverage my skills to develop innovative solutions that address real-world challenges.
+                    I&apos;m an Electrical Engineering graduate from the University of Waterloo, specializing in Artificial Intelligence. My passion for technology drives my continuous exploration of programming languages and frameworks. With experience in security, full-stack development, and robotics, I have contributed to diverse projects, showcasing my ability to innovate and solve complex problems. My academic and practical experiences have equipped me with a strong foundation to contribute effectively to technology-driven environments. My goal is to leverage my skills to develop innovative solutions that address real-world challenges.
                 </div>
             </main>
         </>
