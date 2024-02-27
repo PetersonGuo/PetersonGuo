1bd774b08202bbba23e6966410ac26987d24c3ce
petersonguo <petersonguo@gmail.com>
2024-02-26 22:50:25 -0500

    Updates

--- app/page.js
+++ app/page.js
@@ -58,8 +58,8 @@ export default function Home() {
                         description={<Typing />}
                     />
                 </section>
-                <section className="!mt-0 px-20">
-                    <p>I&apos;m an Electrical Engineering graduate from the University of Waterloo, specializing in Artificial Intelligence. My passion for technology drives my continuous exploration of programming languages and frameworks. With experience in security, full-stack development, and robotics, I have contributed to diverse projects, showcasing my ability to innovate and solve complex problems. My academic and practical experiences have equipped me with a strong foundation to contribute effectively to technology-driven environments. My goal is to leverage my skills to develop innovative solutions that address real-world challenges.</p>
+                <section className="!mt-0 px-[20vw]">
+                    <p>I&apos;m an Electrical Engineering student from the University of Waterloo, specializing in Artificial Intelligence. My passion for technology drives my continuous exploration of programming languages and frameworks. With experience in security, full-stack development, and robotics, I have contributed to diverse projects, showcasing my ability to innovate and solve complex problems. My academic and practical experiences have equipped me with a strong foundation to contribute effectively to technology-driven environments. My goal is to leverage my skills to develop innovative solutions that address real-world challenges.</p>
                 </section>
                 <section className="bg-[#0f0f0f] w-full flex justify-center py-20">
                     <Skills />
