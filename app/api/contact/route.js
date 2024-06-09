4590e9af81aabe655651b90544ddb750d4220621
Peterson Guo <petersonguo@gmail.com>
2024-06-08 21:48:22 -0400

    Update Emailing function

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,18 +1,21 @@
 "use server";
+import { sql } from "@vercel/postgres";
 import { NextResponse } from 'next/server';
 const postmark = require('postmark');
 
 export async function POST(req) {
     try {
         const data = await req.json();
-        console.log(data);
+        const sql_ =
+            await sql`INSERT INTO contact_logging (datetime, email, subject, message) VALUES (NOW(), ${data.email}, ${data.subject}, ${data.message})`;
+
         const client = new postmark.ServerClient(process.env.MAIL_API);
 
         const response = await client.sendEmail({
             "From": "no-reply@petersonguo.com",
             "To": process.env.MAIL_TO,
-            "Subject": `${data.subject} - ${data.email}`,
-            "TextBody": data.message,
+            "Subject": `${data.subject}`,
+            "TextBody": `${data.message} \n\n\n Reply to ${data.email}`,
             "MessageStream": "outbound"
         });
 
