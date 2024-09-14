4959859240ab7b97b9030426bd395ef5a01642ca
Peterson Guo <petersonguo@gmail.com>
2024-09-14 00:27:02 -0400

    .

--- components/Typing.jsx
+++ components/Typing.jsx
@@ -2,37 +2,36 @@
 import React from "react";
 import Typed from "typed.js";
 class Typing extends React.Component {
-  componentDidMount() {
-    const options = {
-      strings: [
-        "I am a Software Engineer",
-        "I am an Electrical Engineer",
-        "I am a Full Stack Developer",
-        "I am an Embedded Systems Developer",
-        "I am a Machine Learning Engineer",
-      ],
-      typeSpeed: 50,
-      backSpeed: 50,
-      loop: true,
-      cursorChar: "|",
-    };
-    this.typed = new Typed(this.el, options);
-  }
-  componentWillUnmount() {
-    this.typed.destroy();
-  }
+	componentDidMount() {
+		const options = {
+			strings: [
+				"I am a Software Engineer",
+				"I am an Electrical Engineer",
+				"I am an Embedded Systems Developer",
+				"I am a Machine Learning Engineer",
+			],
+			typeSpeed: 50,
+			backSpeed: 50,
+			loop: true,
+			cursorChar: "|",
+		};
+		this.typed = new Typed(this.el, options);
+	}
+	componentWillUnmount() {
+		this.typed.destroy();
+	}
 
-  render() {
-    return (
-      <>
-        <span
-          style={{ whiteSpace: "pre" }}
-          ref={(el) => {
-            this.el = el;
-          }}
-        />
-      </>
-    );
-  }
+	render() {
+		return (
+			<>
+				<span
+					style={{ whiteSpace: "pre" }}
+					ref={(el) => {
+						this.el = el;
+					}}
+				/>
+			</>
+		);
+	}
 }
 export default Typing;
