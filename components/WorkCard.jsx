a01000646d344255fb7d8ed8b8436fe3bc940fe9
Peterson Guo <petersonguo@gmail.com>
2025-07-06 22:55:08 -0400

    Update Work card

--- components/WorkCard.jsx
+++ components/WorkCard.jsx
@@ -55,7 +55,7 @@ export default function WorkCard({ children, workData }) {
 						/>
 					</div>
 					<div>
-						<h4 className="text-lg font-bold">{workData.title}</h4>
+						<h4 className="text-lg font-bold">{workData.company} - {workData.title}</h4>
 						<div className="md:flex">
 							<p className="lg:text-md text-sm font-normal">
 								{`${workData.start.toLocaleDateString("en-US", {
