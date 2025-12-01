5b5a240f7e696f8f1b04bbb872347a012a83eec5
Peterson Guo <petersonguo@gmail.com>
2025-12-01 03:05:47 -0500

    Fix contact logging

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -4,25 +4,27 @@ import { NextResponse } from 'next/server';
 const postmark = require('postmark');
 
 export async function POST(req) {
-    try {
-        const data = await req.json();
-        const sql_ =
+    let error = "";
+    const data = await req.json();
+    if (!data.email || !data.subject || !data.message) {
+        return NextResponse.json({ success: false, error: "Missing required fields" });
+    }
+    const sql_ =
             await sql`INSERT INTO contact_logging (datetime, email, subject, message) VALUES (NOW(), ${data.email}, ${data.subject}, ${data.message})`;
-
-        const client = new postmark.ServerClient(process.env.MAIL_API);
-
-        const response = await client.sendEmail({
+    if (sql_.rowCount !== 1) {
+        error += "Database insertion failed";
+    }
+    const client = new postmark.ServerClient(process.env.MAIL_API);
+    const response = await client.sendEmail({
             "From": "no-reply@petersonguo.com",
             "To": process.env.MAIL_TO,
             "Subject": `${data.subject}`,
             "TextBody": `${data.message} \n\n\n Reply to ${data.email}`,
             "MessageStream": "outbound"
         });
-
-        console.log('Email sent successfully:', response);
-        return NextResponse.json({ success: true });
-    } catch (error) {
-        console.error('Error sending email:', error);
-        return NextResponse.json({ success: false, error: error.message });
-    }
+    if (!response.ErrorCode === 0) {
+        error += "Email sending failed";
+    } // Best effort logging
+    console.log('Email sent successfully:', response);
+    return NextResponse.json({ success: true, error: error });
 }
